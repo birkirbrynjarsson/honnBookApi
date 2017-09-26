61073e129d3c85887a08052922c4997f12ffdc5b
sigrung15 <sigrung15@ru.is>
2017-09-26 13:27:40 +0000

    Service class added, but no functionality

--- jaxrs-server/src/main/java/is/ru/honn/service/MemoryUserService.java
+++ jaxrs-server/src/main/java/is/ru/honn/service/MemoryUserService.java
@@ -1,5 +1,52 @@
 package is.ru.honn.service;
 
+import io.swagger.model.AddUserViewModel;
+import io.swagger.model.BookReviewViewModel;
+import io.swagger.model.UserAndBookLoansViewModel;
+import io.swagger.model.UserViewModel;
+
+import java.util.Date;
+import java.util.List;
+
 public class MemoryUserService implements UserService
 {
+    @Override
+    public void addUser(AddUserViewModel body) {
+
+    }
+
+    @Override
+    public void deleteUser(int userId) {
+
+    }
+
+    @Override
+    public List<UserViewModel> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public List<BookReviewViewModel> getBookReviewByUserId(int userId) {
+        return null;
+    }
+
+    @Override
+    public UserAndBookLoansViewModel getUserAndLoanedBooksByDate(Date date) {
+        return null;
+    }
+
+    @Override
+    public UserViewModel getUserById(int userId) {
+        return null;
+    }
+
+    @Override
+    public UserAndBookLoansViewModel getUsersWithMonthOverdueBooksByDate(Date date) {
+        return null;
+    }
+
+    @Override
+    public void updateUser(int userId, AddUserViewModel body) {
+
+    }
 }
--- jaxrs-server/src/main/java/is/ru/honn/service/UserService.java
+++ jaxrs-server/src/main/java/is/ru/honn/service/UserService.java
@@ -1,6 +1,21 @@
 package is.ru.honn.service;
 
-public interface UserService
+import io.swagger.model.AddUserViewModel;
+import io.swagger.model.BookReviewViewModel;
+import io.swagger.model.UserAndBookLoansViewModel;
+import io.swagger.model.UserViewModel;
 
+import java.util.Date;
+import java.util.List;
+
+public interface UserService
 {
+    void addUser(AddUserViewModel body);
+    void deleteUser(int userId);
+    List<UserViewModel> getAllUsers();
+    List<BookReviewViewModel> getBookReviewByUserId(int userId);
+    UserAndBookLoansViewModel getUserAndLoanedBooksByDate(Date date);
+    UserViewModel getUserById(int userId);
+    UserAndBookLoansViewModel getUsersWithMonthOverdueBooksByDate(Date date);
+    void updateUser(int userId, AddUserViewModel body);
 }
